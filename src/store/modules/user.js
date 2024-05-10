48026789791962368dacef9b85e8e16c99556519
riley <riley211375@gmail.com>
2024-05-10 17:23:02 +0800

    feat(custom): 3-19：本地缓存处理方案

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,9 +1,18 @@
 import { login } from '@/api/sys'
 import md5 from 'md5'
+import { setItem, getItem } from '@/utils/storage'
+import { TOKEN } from '@/constant'
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     login(context, userInfo) {
       const { username, password } = userInfo
@@ -13,6 +22,7 @@ export default {
           password: md5(password)
         })
           .then(data => {
+            this.commit('user/setToken', data.data.data.token)
             resolve()
           })
           .catch(err => {
