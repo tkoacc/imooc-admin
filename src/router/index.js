02c8d75963201a3c5f65145c448ef52d3fa4a989
riley <riley211375@gmail.com>
2024-05-11 00:13:05 +0800

    feat(custom): 3-22：登录鉴权解决方案

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const publicRoutes = [{
-  path: '/login',
-  component: () => import('@/views/login/index')
-}]
+const publicRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index')
+  },
+  {
+    path: '/',
+    component: () => import('@/layout/index')
+  }]
 
 const router = createRouter({
   history: createWebHashHistory(),
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,6 +2,7 @@ import { login } from '@/api/sys'
 import md5 from 'md5'
 import { setItem, getItem } from '@/utils/storage'
 import { TOKEN } from '@/constant'
+import router from '@/router'
 export default {
   namespaced: true,
   state: () => ({
@@ -23,6 +24,8 @@ export default {
         })
           .then(data => {
             this.commit('user/setToken', data.token)
+            // 登录后操作
+            router.push('/')
             resolve()
           })
           .catch(err => {
