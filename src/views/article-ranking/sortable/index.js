57264634beb0029d708e147e5740943b708f7dbd
riley <riley211375@gmail.com>
2024-05-22 18:53:34 +0800

    feat(custom): 9-13：辅助业务：文章详情展示

---
+++ src/views/article-ranking/sortable/index.js
@@ -0,0 +1,56 @@
+import { ref, nextTick } from 'vue'
+import Sortable from 'sortablejs'
+import { articleSort } from '@/api/article'
+import { ElMessage } from 'element-plus'
+import i18n from '@/i18n'
+
+// 排序相关
+export const tableRef = ref(null)
+
+/**
+ * 初始化排序
+ */
+export const initSortable = (tableData, cb) => {
+  nextTick(() => {
+    const bodyWrapper = tableRef.value.$el.querySelector('.el-table__body-wrapper')
+    if (!bodyWrapper) {
+      console.error('.el-table__body-wrapper not found')
+      return
+    }
+
+    const table = bodyWrapper.querySelector('table')
+    if (!table) {
+      console.error('table not found')
+      return
+    }
+
+    const tbody = table.querySelector('tbody')
+    if (!tbody) {
+      console.error('tbody not found')
+      return
+    }
+
+    // 设置拖拽效果
+    Sortable.create(tbody, {
+      // 拖拽时类名
+      ghostClass: 'sortable-ghost',
+      // 拖拽结束的回调方法
+      async onEnd(event) {
+        const { newIndex, oldIndex } = event
+        // 修改数据
+        await articleSort({
+          initRanking: tableData.value[oldIndex].ranking,
+          finalRanking: tableData.value[newIndex].ranking
+        })
+        ElMessage.success({
+          message: i18n.global.t('msg.article.sortSuccess'),
+          type: 'success'
+        })
+        // 直接重新获取数据无法刷新 table！！
+        tableData.value = []
+        // 重新获取数据
+        cb && cb()
+      }
+    })
+  })
+}
